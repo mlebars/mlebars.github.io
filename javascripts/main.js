b3e6f3e92f9e8d87d99c7526125d207ea443fd72
Marie <mlebars@users.noreply.github.com>
2016-05-29 15:11:12 +0100

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,3 @@
-console.log('This would be the main JS file.');
-
 // Google Analytics
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
