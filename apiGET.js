4b4dbab813162b967de287512d3084d4bb2c2da6
Marie <mlebars@users.noreply.github.com>
2024-05-17 11:30:24 +0100

    Update apiGET.js

--- apiGET.js
+++ apiGET.js
@@ -1,4 +1,4 @@
-const myHeaders = new Headers();
+.const myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 myHeaders.append("Authorization", "bearer cOmUpOXfevWx560egh1njqp2a6d0YkozFUQA3bgoi4m1t6AiPGBq1BNpPS-qkLy8EnAzYZdjRS6t-4SUUsV3YLvA2N0sB3PJHMw9VEZ3jILEZRtXkrOoiv3FQpXxYw5c");
 myHeaders.append("Cookie", "attr_multitouch=\"d68ZnthUwK5yHWYjwJDSLN7kLe4=\"; cdp_seg=\"RTvTqnsaYEnEQS5xUFs7jaagUus=\"; ep201=\"ruWnNqv2Rcg/S09mhNQzlQ0gl/s=\"; ep202=\"Ho8rOLNNhXN+6PaSHbzBaQ8wVew=\"; ep203=\"4UdjPNj2cIMU/P81GKM+STrI2S8=\"");
@@ -29,8 +29,9 @@ function printSurveys(mySurveys){
 	var mySurveysData = mySurveys['data'];
 	var text = "<table><tr><th>Survey Title</th><th>Response(s)</th></tr>";
 	for (let i = 0; i < mySurveysData.length; i++) {
-	  	text += '<tr><td onclick=getSurveyID("'+mySurveysData[i]['id']+'");>'+mySurveysData[i]['title']+'</td><td>'+mySurveysData[i]['response_count'];
+	  	text += '<tr><td onclick=getSurveyID("'+mySurveysData[i]['id']+'");>'+mySurveysData[i]['title']+'</td><td>'+mySurveysData[i]['response_count']+'</td></tr>';
 		i += 1;
 	}
-	getElementById('postAuth').innerHTML = text;
+	text += '</table>';
+	document.getElementById('postAuth').innerHTML = text;
 }
