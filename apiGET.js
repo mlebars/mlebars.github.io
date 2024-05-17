2bde7d374b9fb37785eee3eb658aae564b443dfb
Marie <mlebars@users.noreply.github.com>
2024-05-17 09:33:36 +0100

    Update apiGET.js

--- apiGET.js
+++ apiGET.js
@@ -22,6 +22,7 @@ function getSurveys(){
 			//console.log(mySurveys);
 		});
 }
+console.log(mySurveys);
 for (let i = 0; i < mySurveys['data'].length; i++) {
   	//text += cars[i] + "<br>";
 	console.log(mySurveys['data'][i]['title']);
