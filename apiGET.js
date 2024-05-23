597b7a7dd4762d293c76b1b2924d25ea05777fef
Marie <mlebars@users.noreply.github.com>
2024-05-23 10:01:55 +0100

    Update apiGET.js

--- apiGET.js
+++ apiGET.js
@@ -1,18 +1,16 @@
-const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
-myHeaders.append("Authorization", "bearer cOmUpOXfevWx560egh1njqp2a6d0YkozFUQA3bgoi4m1t6AiPGBq1BNpPS-qkLy8EnAzYZdjRS6t-4SUUsV3YLvA2N0sB3PJHMw9VEZ3jILEZRtXkrOoiv3FQpXxYw5c");
-myHeaders.append("Cookie", "attr_multitouch=\"d68ZnthUwK5yHWYjwJDSLN7kLe4=\"; cdp_seg=\"RTvTqnsaYEnEQS5xUFs7jaagUus=\"; ep201=\"ruWnNqv2Rcg/S09mhNQzlQ0gl/s=\"; ep202=\"Ho8rOLNNhXN+6PaSHbzBaQ8wVew=\"; ep203=\"4UdjPNj2cIMU/P81GKM+STrI2S8=\"");
-
-const requestOptions = {
-	method: "GET",
-  	headers: myHeaders,
-  	redirect: "follow"
-};
-
 let mySurveys = null;
 getSurveys();
 
-function getSurveys(){
+function getSurveys(access_token){
+	const myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	myHeaders.append("Authorization", "bearer "+access_token;
+	
+	const requestOptions = {
+		method: "GET",
+	  	headers: myHeaders,
+	  	redirect: "follow"
+	};
 	fetch("https://api.surveymonkey.com/v3/surveys?include=response_count&sort_by=num_responses&sort_order=DESC", requestOptions)
 		.then(function(response) {
 			return response.json();
