730697efb6d62d3a5562935b588a0144e3c59c8a
Marie <mlebars@users.noreply.github.com>
2024-05-23 11:01:50 +0100

    Update apiGET.js

--- apiGET.js
+++ apiGET.js
@@ -2,15 +2,13 @@ let mySurveys = null;
 
 function getSurveys(access_token){
 	var requestOptions = getRequestOptions(access_token);
-	console.log(requestOptions);
-	console.log(JSON.stringify(requestOptions));
 	fetch("https://api.surveymonkey.com/v3/surveys?include=response_count&sort_by=num_responses&sort_order=DESC", requestOptions)
 		.then(function(response) {
 			return response.json();
 		})
 		.then(function(data) {
 			mySurveys = data;
-			printSurveys(mySurveys,requestOptions);
+			printSurveys(mySurveys,access_token);
 		});
 }
 
@@ -27,20 +25,20 @@ function getRequestOptions(access_token) {
 	return requestOptions;
 }
 
-function printSurveys(mySurveys,requestOptions){
+function printSurveys(mySurveys,access_token){
 	//var requestOptions = requestOptions;
 	var mySurveysData = mySurveys['data'];
 	var text = "<table><tr><th>Survey Title</th><th>Response(s)</th></tr>";
 	for (let i = 0; i < mySurveysData.length; i++) {
-	  	text += '<tr><td class="surveyTitle" onclick=getSurveyID("'+mySurveysData[i]['id']+'",'+requestOptions+');>'+mySurveysData[i]['title']+'</td><td class="surveyResponses">'+mySurveysData[i]['response_count']+'</td></tr>';
+	  	text += '<tr><td class="surveyTitle" onclick=getSurveyID("'+mySurveysData[i]['id']+'",'+access_token+');>'+mySurveysData[i]['title']+'</td><td class="surveyResponses">'+mySurveysData[i]['response_count']+'</td></tr>';
 	}
 	text += '</table>';
 	document.getElementById('postAuth').innerHTML = text;
 }
 
-function getSurveyID(surveyID,requestOptions) {
+function getSurveyID(surveyID,access_token) {
 	console.log(surveyID);
-	//var requestOptions = getRequestOptions(access_token);
+	var requestOptions = getRequestOptions(access_token);
 	fetch("https://api.surveymonkey.com/v3/surveys/"+surveyID+"/responses/bulk", requestOptions)
 		.then((response) => response.text())
 		.then((result) => console.log(result))
